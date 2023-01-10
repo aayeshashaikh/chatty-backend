43fd4098c955bf9247d7319b2090fd5e6765d9a3
aayeshaManek <119667960+aayeshaManek@users.noreply.github.com>
2023-01-10 12:21:10 +0530

    setup project

---
+++ src/setupDatabase.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { config } from "./config";
+import Logger from "bunyan";
+
+const log: Logger = config.createLogger('setupDatabase');
+
+export default () => {
+    const connect = () => {
+        mongoose.set("strictQuery", false);
+        mongoose.connect(`${config.DATABASE_URL}`)
+            .then(() => {
+                log.info("Successfully connected to DB")
+            })
+            .catch((err) => {
+                log.error("Error connecting to DB", err);
+                return process.exit(1);
+            })
+    };
+    connect();
+
+    mongoose.connection.on('Disconnected', connect)
+};
